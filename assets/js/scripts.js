6a2faa827333930fa8a224abb45423b675381b3d
Benie <davinci.monalissa@gmail.com>
2022-03-21 19:09:47 +0300

    Initialize by hiding the Print Orders table and checkout buttons.

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+// Initialize
+// Hide Print Orders table and checkout buttons
+$(document).ready(function() {
+  $(".table").hide();
+  $(".table h2").hide();
+  $(".checkout-buttons").hide();
+  $(".checkout-info").hide();
+  $(".btn.yes").hide();
+  $(".btn.no").hide();
+  $(".checkout-info h4").hide();
+  $(".delivery-image").hide();
+
+  
